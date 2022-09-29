cda9b21cebe1893395f85b15936493222704f29e
erwinsanjaya165 <erwinsanjaya165@gmail.com>
2022-09-29 07:27:38 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, {Component} from 'react';
-import {NavigationContainer, DarkTheme} from '@react-navigation/native';
-import {
-  Provider as PaperProvider,
-  DarkTheme as PaperDarkTheme,
-} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
 import Navigation from './router';
 
 export default class App extends Component {
   render() {
     return (
-      <PaperProvider>
-        <NavigationContainer>
-          <Navigation />
-        </NavigationContainer>
-      </PaperProvider>
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
     );
   }
 }
